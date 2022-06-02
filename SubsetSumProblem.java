444a47324725535618ea925e7be80f9b2526e135
shivangbelwariar96 <97721882+shivangbelwariar96@users.noreply.github.com>
2022-06-02 06:43:54 +0530

    Update SubsetSumProblem.java

--- SubsetSumProblem.java
+++ SubsetSumProblem.java
@@ -2,6 +2,74 @@ package package1;
 
 // https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
 
+
+
+FIRST MEHTOD(Better):
+NOTE that for Questions involving BOOLEAN in Memoization DP, Use 2 T[][], first should be a boolean T[][] to store results of subproblems (of sub-recursions)  
+	& USE another T (T2[][]) for marking if the recursion is already traversed or not. If already traversed (T2[][] value is not -1), return T[][] else, mark T2[][]
+	and assign T[][] by making boolean result recursive calls.
+
+	public static boolean subSetSumProblem(int arr[], int sum, int end, boolean T2[][], int T[][]) {
+		
+		if(end < 0) {
+			
+			return false;
+		}
+		
+		if(sum == 0) {
+			
+			return true;
+		}
+		
+		if(sum < 0) {
+			
+			return false;
+		}
+		
+		if(T[sum][end] != -1) {
+			
+			return T2[sum][end];
+		}
+		
+		T[sum][end] = 1;
+		
+		return T2[sum][end] = subSetSumProblem(arr, sum-arr[end], end-1, T2, T)
+				|| subSetSumProblem(arr, sum, end-1, T2, T);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int arr[] = {3, 34, 4, 12, 5, 2};
+		int sum = 9;
+		
+		int T[][] = new int[1000][1000];
+		
+		boolean T2[][] = new boolean[1000][1000];
+		
+		for(int i=0; i<1000; i++) {
+			
+			for(int j=0; j<1000; j++) {
+				
+				T[i][j] = -1;
+			}
+		}
+		
+		System.out.println(subSetSumProblem(arr, sum, arr.length-1, T2, T));
+	}
+
+
+
+
+
+
+
+
+SECOND METHOD:
+
+
 public class SubsetSumProblem {
 	
 	static int flag = 0;
