633760d81dae1bc0fb69c246f513e1fb39e798e0
shivangbelwariar96 <97721882+shivangbelwariar96@users.noreply.github.com>
2025-04-19 21:14:23 -0700

    Update CuttingARod.java

--- CuttingARod.java
+++ CuttingARod.java
@@ -1,3 +1,58 @@
+// Java program to find maximum
+// profit from rod of size n 
+
+import java.util.*;
+
+class GfG {
+
+    static int cutRodRecur(int i, int[] price, int[] memo) {
+        
+        // Base case
+        if (i == 0) return 0;
+        
+        // If value is memoized
+        if (memo[i - 1] != -1) return memo[i - 1];
+        
+        int ans = 0;
+
+        // Find maximum value for each cut.
+        // Take value of rod of length j, and 
+        // recursively find value of rod of 
+        // length (i-j).
+        for (int j = 1; j <= i; j++) {
+            ans = Math.max(ans, price[j - 1] + cutRodRecur(i - j, price, memo));
+        }
+
+        return memo[i - 1] = ans;
+    }
+
+    static int cutRod(int[] price) {
+        int n = price.length;
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return cutRodRecur(n, price, memo);
+    }
+
+    public static void main(String[] args) {
+        int[] price = {1, 5, 8, 9, 10, 17, 17, 20};
+        System.out.println(cutRod(price));
+    }
+}
+
+
+
+
+
+____________________________________
+
+
+
+
+
+
+
+
+
 package package1;
 
 // https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
